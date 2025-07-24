fdd50213f0e42fa7de755a4e469d76980ad2a650
alina2002200 <71118359+alina2002200@users.noreply.github.com>
2025-07-24 18:59:15 +0300

    Update and rename lithosim_dots_vs_zernike_dots.py to dots_vs_lithosim_dots1.py

--- additional/dots_vs_lithosim_dots1.py
+++ additional/dots_vs_lithosim_dots1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img1 = cv2.imread('/content/t1_0_mask_ress.png', cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread('/content/lithosim/t1_0_mask_ress.png', cv2.IMREAD_GRAYSCALE)
 img2 = cv2.imread('/content/lithosim/output/refine_litho_out/t1_0_mask.png', cv2.IMREAD_GRAYSCALE)
 
 diff = cv2.absdiff(img1, img2)
